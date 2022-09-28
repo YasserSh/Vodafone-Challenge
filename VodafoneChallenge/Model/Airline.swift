60640161dcff5229bb75ad61b0d783100c84e9a2
Yasser <Abouelfotouh.yasser@gmail.com>
2022-09-28 14:27:01 +0200

    add airline model

---
+++ VodafoneChallenge/Model/Airline.swift
@@ -0,0 +1,22 @@
+//
+//  Airline.swift
+//  VodafoneChallenge
+//
+//  Created by Vodafone Mac on 28/09/2022.
+//
+
+import Foundation
+
+class Airline {
+    let name : String
+    let slogan : String
+    let country : String
+    let headquarters : String
+    
+    init (name: String, slogan: String, country: String, headquarters: String){
+        self.name = name
+        self.slogan = slogan
+        self.country = country
+        self.headquarters = headquarters
+    }
+}
